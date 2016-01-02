e25ce59afd9269ec0584dbfc9ba9bf08a9c4d1b5
pajowu <github@ca.pajowu.de>
2016-01-02 19:39:46 +0200

    its not a good idea to forget code

---
+++ gsw/backend/exceptions.py
@@ -0,0 +1,5 @@
+from rest_framework.exceptions import APIException
+
+class ServiceUnavailable(APIException):
+    status_code = 503
+    default_detail = 'Service temporarily unavailable, try again later.'
