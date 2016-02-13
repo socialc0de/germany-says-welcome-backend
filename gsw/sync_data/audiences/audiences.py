f418a014a5a1f4896742d21742c7029950fe8ba6
pajowu <github@ca.pajowu.de>
2016-02-13 17:28:03 +0200

    added audience init script

---
+++ gsw/sync_data/audiences/audiences.py
@@ -0,0 +1,13 @@
+import django
+django.setup()
+from backend.models import Audience
+langs = ["en","de","fr","ar"]
+steps = ["Step 1 %s", "Step 2 %s", "Step 3 %s"]
+for step in steps:
+	audience = Audience()
+	for lang in langs:
+		audience.translate(lang)
+		audience.name = step%lang
+		audience.description = step%lang
+	audience.save()
+print(Audience.objects.all())
