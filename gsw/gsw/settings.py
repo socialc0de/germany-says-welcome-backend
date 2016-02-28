d6a400cd5e81cf09b9630cbfeee6a566597a662a
pajowu <github@ca.pajowu.de>
2016-02-28 22:38:38 +0200

    bugfixing

--- gsw/gsw/settings.py
+++ gsw/gsw/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'gsw.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
