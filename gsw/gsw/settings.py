06d4db58a0ff918d77e2448f3291e9a7fe778380
pajowu <github@ca.pajowu.de>
2016-03-03 16:01:30 +0200

    make clean that secret key is ONLY FOR TESTING

--- gsw/gsw/settings.py
+++ gsw/gsw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=f4aydus9$f*c(108mqo!-)b8i@ttb80&h%940z@4bd%)%i8jj'
+SECRET_KEY = 'test_=f4aydus9$f*c(108mqo!-)b8i@ttb80&h%940z@4bd%)%i8jj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
